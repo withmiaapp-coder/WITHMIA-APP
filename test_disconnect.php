74e804084bf5e8d394ee4e90c382830824081ee1
withmiaapp-coder <withmia.app@gmail.com>
2025-12-07 13:49:58 -0300

    refactor: elimina archivos de backup, documentación temporal y scripts obsoletos

--- test_disconnect.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$manager = new App\Services\EvolutionInstanceManager();
-var_dump(method_exists($manager, 'disconnectInstance'));
